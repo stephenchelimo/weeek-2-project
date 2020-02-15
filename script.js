161a7ad4d193cd49f37f78784cbc2111de3ef9fc
stephen chelimo <stephenchelimo057@gmail'com>
2020-02-15 23:04:59 +0300

    functions declarations

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ var femaleNames = {
     Friday: 'Afua',
     Saturday: 'Ama',
 };
+function ClearFields() {
+
+    document.getElementById("d").value = "";
+    document.getElementById("m").value = "";
+    document.getElementById("yy").value = "";
+    document.getElementsByName("gd").value = "";
+    
+    document.getElementById('one').style.display = "none";
+    document.getElementById('two').style.display = "none";
+    document.getElementById('three').style.display = "none";
+    document.getElementById('four').style.display = "none";
+    document.getElementById("ans").style.display = "none";
+}
+function validation() {
+    var date = document.getElementById("d").value;
+    
+    var month = document.getElementById("m").value;
+    var year = document.getElementById("yy").value;
+    var gen = document.getElementsByName("gd");
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    if(isNaN(date) || date === null || date === "" || date < 1 || date > 31){
+       
+        document.getElementById('one').innerHTML = "invalid day";
+        document.getElementById('one').style.color = "red"
+        alert("invalid date");
+        return false;
+    }
