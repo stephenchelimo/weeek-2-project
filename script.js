8a29711a82507e6d7f3d95fcfd741d6d72c06d38
stephen chelimo <stephenchelimo057@gmail'com>
2020-02-19 09:18:16 +0300

    changes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
 function results() {
     var month =parseInt(document.getElementById("month").value);
     var year =parseInt(document.getElementById("year").value);
     var day=parseInt(document.getElementById("day").value);
     var date=new Date(year + "/" + month + "/" + day);
+
     var gender= print();
     var day2=date.getDay();
+
     var akan;
     if(day <1 || day >31){
         alert("invalid date");
@@ -22,14 +25,16 @@ return false;
             return false;
     }
     if(gender === "male") {
-        akan =akanMale[day1];
-    }else{
-            akan =akanFemale[day1];
+        akan =akanMale[day2];
+    }else
+    {
+            akan =akanFemale[day2];
     }
             alert("You were born on " + dayOfTheWeek [day2] + "  and your akan name is " + akan);
 }
 function print(){
     var gender=document.getElementsByName("gender");
+    
     for(i=0; i<gender.length; i++){
         if(gender[i].checked){
             return(gender[i].value)
